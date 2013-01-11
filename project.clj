fe476098d501f75da9c4ef705e08d2f3338da04f
josephwilk <joe@josephwilk.net>
2013-01-11 15:16:34 +0100

    Clojure skelelton

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject computer-models-of-musical-creativity "0.1.0-SNAPSHOT"
+  :description "Examples from David Copes Computer Models of Musical Creativity book"
+  :url "http://github.com/josephwilk/computer-models-of-musical-creativity"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
