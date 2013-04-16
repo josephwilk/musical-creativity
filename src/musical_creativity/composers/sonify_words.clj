754967ccc446b9b239e4a54564916ffe53980522
Joseph Wilk <joe@josephwilk.net>
2013-04-16 17:52:02 +0200

    Deal with numbers

--- src/musical_creativity/composers/sonify_words.clj
+++ src/musical_creativity/composers/sonify_words.clj
@@ -6,22 +6,25 @@
 (def numbers [50, 53, 56, 59, 62, 65, 68, 71, 74, 77])
 (def punctuation-numbers [46, 44, 39, 33, 63, 40, 41, 91, 93, 123, 125, 35, 61, 95, 60, 62, 34, 45, 43, 47, 92])
 
+(defn code-as-number [character-code]
+  (Integer. (str (char character-code))))
+
 (defn find-pitch [character-code]
   (cond
-    (and (>= character-code 65) (<= character-code 90))
+    (<= 65 character-code 90)
     (+ character-code transpose)
-  
-    (and (>= character-code 97) (<= character-code 122))
+
+    (<= 97 character-code 122)
     (+ transpose (- character-code 47))
-  
+
     (= character-code 32)
     32
-    
-    (and (>= character-code 48) (<= character-code 57))
-    (+ transpose (nth character-code numbers))
-    
+
+    (<= 48 character-code 57)
+    (+ transpose (nth numbers (code-as-number character-code)))
+
     (some #{character-code} punctuation-numbers)
-    (60)))
+    60))
 
 (defn sonify-character [character]
   (let [character-code (int character)
