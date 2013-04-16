39e8ae0386c35b7d2e9e3a0813c405aa8bfca226
Joseph Wilk <joe@josephwilk.net>
2013-04-16 17:52:14 +0200

    More flexible with different kind of events

--- src/musical_creativity/events.clj
+++ src/musical_creativity/events.clj
@@ -1,14 +1,19 @@
 (ns musical-creativity.events)
 
-(defn random-duration []
+(defn- random-duration []
   (+ 250 (rand-int 800)))
 
-(defn random-channel []
+(defn- random-channel []
   (+ 1 (rand-int 16)))
+
+(defn- extract-pitch [pitch]
+  (if (map? pitch)
+    (:pitch pitch)
+    pitch))
 
 (defn make-event [ontime pitch & [channel]]
   {:time ontime
-   :pitch (:pitch pitch)
+   :pitch (int (extract-pitch pitch))
    :channel (or channel 1)})
 
 (defn random-make [pitch-groupings & [ontime]]
@@ -19,10 +24,10 @@
         (concat [(make-event ontime (first pitch-groupings) (random-channel))]
                  (random-make (rest pitch-groupings) (+ ontime duration)))))))
 
-(defn make [pitch-groupings & [ontime]]
+(defn make [pitch-groupings & [ontime interval]]
   (let [ontime (or ontime 0)]
     (if (empty? pitch-groupings) 
       []
-      (let [duration 200]
+      (let [duration (or interval 300)]
         (concat [(make-event ontime (first pitch-groupings))]
-                 (make (rest pitch-groupings) (+ ontime duration)))))))
+                 (make (rest pitch-groupings) (+ ontime duration) interval))))))
