ee93c1f190ef95e3ce3cd9cb2fef814ef3575274
josephwilk <joe@josephwilk.net>
2013-04-17 12:43:15 +0200

    Only log when there is log.

--- src/musical_creativity/instruments.clj
+++ src/musical_creativity/instruments.clj
@@ -29,7 +29,7 @@
         log (:log event)
         note-time (+ start-time (:time event))]
     (at note-time (player-fn pitch-to-play))
-    (overtone.at-at/at (- note-time 10) #(do (print log) (flush)) my-pool)))
+    (overtone.at-at/at (- note-time 10) #(when log (do (print log) (flush))) my-pool)))
 
 (defn play-saw [event start-time]
   (let [pitch (:pitch event)
