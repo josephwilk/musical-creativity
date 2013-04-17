335d4b3af6a99827e708dfefbeff895ef3e5ae9b
Joseph Wilk <joe@josephwilk.net>
2013-04-17 20:34:19 +0200

    it compiled

--- src/musical_creativity/composers/fuzzy.clj
+++ src/musical_creativity/composers/fuzzy.clj
@@ -52,132 +52,7 @@
 (def PC_BFLAT 10)
 (def PC_B 11)
 
-(defn sumup
-  "Adds the members of a list."
-  [the-list]
-  (if (empty? the-list)
-    0
-    (+ (first the-list) (sumup (rest the-list)))))
-
-(defn top_n_positions
-  "Returns positions of highest n values in list, or nil if all 0."
-  [the-list howmany]
-    (if (= 0 howmany)  ; finished
-      nil
-      (if (nil? (sumup the-list)) ; empty list
-        nil
-        (let [alist nil]
-          (setf alist (copy-list the-list))  ;   copy input list
-          (sort
-           (cons
-            (let [i 0
-                  k 0
-                  hsf 0] ;  hsf is highest so far
-              (dolist (x alist)
-                      (when (> x hsf)
-                        (setf hsf x k i))
-                      (setf i (+ i 1)))
-              (setf (elt alist k) 0)
-              k) ; k is consed
-            (top_n_positions alist (- howmany 1)))
-           #'<)))))
-
-(defn last_sol_test
-  "Based on *last_sol*."
-  []
-  (let [test (first (top_n_positions *last_sol* 1))]
-    (cond
-     ((= 0 test) '(0 1 1))
-     ((= 1 test) '(1 1 0))
-     (t '(1 0 0)))))
-
-(defn make_set [pitch_list]
-  (if (empty? pitch_list) [0 0 0 0 0 0 0 0 0 0 0 0]
-      (fz_union (ror_n *fz_note* (first pitch_list))
-                (make_set (rest pitch_list)))))
-
-(defn pick_chord_w_more_rules [thenote]
-    "Returns pitch-classes of a chord."
-    (let [the_pc (rem thenote 12)
-          solSet [0 0 0]]
-      (reset! *as_root_set* (make_set (as_root the_pc)))
-      (reset! *as_third_set* (make_set (as_third the_pc)))
-      (reset! *as_fifth_set* (make_set (as_fifth the_pc)))
-      (reset! solSet (add_lists solSet (common_tones_test)))
-      (reset! solSet (add_lists solSet (last_sol_test)))
-      (reset! solSet (add_lists solSet (favor_root_for_tonic the_pc)))
-      (reset! solSet (add_lists solSet (dither)))
-      (case (ltop (reset! *last_sol* solset))
-        (2 (as_fifth the_pc))
-        (1 (as_third the_pc))
-        (otherwise (as_root the_pc)))))
-
-(defn pick_and_play_more_rules_chord [thenote time]
-    "Returns a chord in event notation."
-    (make_chord_event
-     (add_oct
-      (pick_chord_w_more_rules (rem thenote 12)) 4) time))
-
-(defn fuzzy
-  ([pcs] (fuzzy pcs 0))
-  ([pcs time]
-     (if (empty? pcs) []
-         (concat (pick_and_play_more_rules_chord (first pcs) time)
-                 (fuzzy (rest pcs) (+ time 1000))))))
-
-
-(defn add_lists [list1 list2]
-   "Add two lists, member by member"
-     (mapcar #'+ list1 list2))
-
-(defn ror_n
-  "Rotates 12 element list n steps to right."
-  [lst n]
-  (let [shift_by (- 12 n)]
-      (concat (subseq lst shift_by) (subseq lst 0 shift_by))))
-
-(defn fz_union
-  "Returns fuzzy union (maximums) of any number of lists"
-  [&rest theLists]
-     (fz_u theLists))
-
-(defn favor_root_for_tonic
-  "Gives slight edge to root position for tonic"
-  [thePC]
-   (if (zerop thePC) '(0.1 0 0) '(0 0 0)))
-
-(defn dither
-  "Random tie breaker"
-  []
-  (let [choice (random 3.0)]
-    (cond
-     ((< choice 1) '(0.05 0 0))
-     ((< choice 2) '(0 0.05 0))
-     (t '(0 0 0.05)))))
-
-(defn common_tones_test
-  "Applies common tone rules."
-  []
-  (common_tone_rules *as_root_set* *as_third_set* *as_fifth_set* *old_chord_set*))
-
-(defn common_tone_rules
-  "Returns set according to common tone rules."
-  [set1 set2 set3 master]
-  (let [result [0 0 0]]
-    (setf (first result) (common_tones set1 master))
-    (setf (second result) (common_tones set2 master))
-    (setf (third result) (common_tones set3 master))
-    result))
-
-(defn common_tones
-  "Sums the set intersections."
-  [set1 set4]
-  (sumup (fz_intersect set1 set4)))
-
-(defn ltop
-  "Returns the topmost position of the list."
-  [thelist]
-  (first (top_n_positions thelist 1)))
+(declare common_tone_rules)
 
 (defn fz_complement
   "Returns fuzzy complement (1 - membership) of a list."
@@ -196,56 +71,31 @@
     (cons (minumum (first lst1) (first lst2))
           (fz_intersect (rest lst1) (rest lst2)))))
 
-(defn maximum
-  "Returns the maximum of its two args."
-  [x y]
-  (if (and (realp x) (realp y))
-    (max x  y)
-    0))
 
-(defn minumum
-  "Returns the minimum of its two args."
-  [x y]
-  (if (and (realp x) (realp y))
-    (min x  y)
-    0))
+(defn common_tones_test
+  "Applies common tone rules."
+  []
+  (common_tone_rules *as_root_set* *as_third_set* *as_fifth_set* *old_chord_set*))
 
-(defn fz_un
-  "Returns fuzzy union (maximums) of two lists."
-  [lst1 lst2]
-  (if (empty? lst1)
-    nil
-    (cons (maximum (first lst1) (first lst2))
-          (fz_union (rest lst1) (rest lst2)))))
+(defn common_tones
+  "Sums the set intersections."
+  [set1 set4]
+  (sumup (fz_intersect set1 set4)))
 
-(defn fz_u
-  "Returns fuzzy union (maximums) of many lists."
-  [theLists]
-  (if (empty? (rest theLists))
-    (first thelists)
-    (fz_un (first theLists) (fz_u (rest theLists)))))
+(defn common_tone_rules
+  "Returns set according to common tone rules."
+  [set1 set2 set3 master]
+  (let [result [0 0 0]
+        result (assoc result 0 (common_tones set1 master))
+        result (assoc result 1 (common_tones set2 master))
+        result (assoc result 2 (common_tones set3 master))]
+    result))
 
-(defn ascend_list
-  "Adds 12 to pitches lower than first one keeps chords in root position."
+
+(defn ltop
+  "Returns the topmost position of the list."
   [thelist]
-  (let [root (first thelist)]
-    (mapcar #'(lambda (x) (if (< x root) (+ x 12) x))
-            thelist)))
-
-(defn add_oct
-  "Adds octave to the list."
-  ([theList] (add_oct theList 4))
-  ([theList theOctave]
-     (mapcar #'(lambda (x) (+ x (* theoctave 12)))
-             (ascend_list theList))))
-
-(defn make_chord_event
-  "Simple notelist to chord."
-  ([pitch_list] (make_chord_event pitch_list 0))
-  ([pitch_list time]
-      (if (empty? pitch_list) []
-          (cons (list time (first pitch_list) *default_dur* *default_ch* *default_vel*)
-                (make_chord_event (rest pitch_list) time)))))
+  (first (top_n_positions thelist 1)))
 
 (defn third_above
   "Returns a third above the note."
@@ -267,11 +117,6 @@
   [thenote]
   (first (top_n_positions (fz_intersect *current_scale* (ror_n *fz_5b* thenote)) 1)))
 
-(defn as_root
-  "Returns pitch-class chord with arg as root."
-  [thenote]
-  (list thenote (third_above thenote) (fifth_above thenote)))
-
 (defn as_third
   "Returns pitch-class chord with arg as third."
   [thenote]
@@ -281,3 +126,168 @@
   "Returns pitch-class chord with arg as fifth."
   [thenote]
   (list (fifth_below thenote) (third_above (fifth_below thenote)) thenote))
+
+(defn ascend_list
+  "Adds 12 to pitches lower than first one keeps chords in root position."
+  [thelist]
+  (let [root (first thelist)]
+    (map (fn [x] (if (< x root) (+ x 12) x))
+            thelist)))
+
+(defn ror_n
+  "Rotates 12 element list n steps to right."
+  [lst n]
+  (let [shift_by (- 12 n)]
+      (concat (subseq lst shift_by) (subseq lst 0 shift_by))))
+
+(defn sumup
+  "Adds the members of a list."
+  [the-list]
+  (if (empty? the-list)
+    0
+    (+ (first the-list) (sumup (rest the-list)))))
+
+(defn top_n_positions
+  "Returns positions of highest n values in list, or nil if all 0."
+  [the-list howmany]
+    (if (= 0 howmany)  ; finished
+      nil
+      (if (nil? (sumup the-list)) ; empty list
+        nil
+        (let [alist the-list]
+;          (dosync (alter  alist (copy-list the-list)))  ;   copy input list
+          (sort
+           (cons
+            (let [i (ref 0)
+                  k (ref 0)
+                  hsf (ref 0)] ;  hsf is highest so far
+              (doseq [x alist]
+                (when (> x hsf)
+                  (dosync (alter hsf x))
+                  (dosync (alter k i)))
+                (dosync (alter i inc)))
+
+              (dosync (alter alist (assoc alist k 0)))
+              k) ; k is consed
+            (top_n_positions alist (- howmany 1)))
+           #'<)))))
+
+(defn last_sol_test
+  "Based on *last_sol*."
+  []
+  (let [test (first (top_n_positions *last_sol* 1))]
+    (cond
+     (= 0 test) '(0 1 1)
+     (= 1 test) '(1 1 0)
+     :else
+     '(1 0 0))))
+
+(defn maximum
+  "Returns the maximum of its two args."
+  [x y]
+  (if (and x y)
+    (max x  y)
+    0))
+
+(defn minumum
+  "Returns the minimum of its two args."
+  [x y]
+  (if (and x y)
+    (min x  y)
+    0))
+
+(defn fz_un
+  "Returns fuzzy union (maximums) of two lists."
+  [lst1 lst2]
+  (if (empty? lst1)
+    nil
+    (cons (maximum (first lst1) (first lst2))
+          (fz_union (rest lst1) (rest lst2)))))
+(defn fz_u
+  "Returns fuzzy union (maximums) of many lists."
+  [the-lists]
+  (if (empty? (rest the-lists))
+    (first the-lists)
+    (fz_un (first the-lists) (fz_u (rest the-lists)))))
+
+(defn fz_union
+  "Returns fuzzy union (maximums) of any number of lists"
+  [&rest the-lists]
+     (fz_u the-lists))
+
+
+(defn make_set [pitch_list]
+  (if (empty? pitch_list) [0 0 0 0 0 0 0 0 0 0 0 0]
+      (fz_union (ror_n *fz_note* (first pitch_list))
+                (make_set (rest pitch_list)))))
+
+(declare third_above fifth_above)
+
+(defn as_root
+  "Returns pitch-class chord with arg as root."
+  [thenote]
+  (list thenote (third_above thenote) (fifth_above thenote)))
+
+(defn add_lists [list1 list2]
+   "Add two lists, member by member"
+     (map + list1 list2))
+
+(defn favor_root_for_tonic
+  "Gives slight edge to root position for tonic"
+  [thePC]
+   (if (= 0 thePC) '(0.1 0 0) '(0 0 0)))
+
+(defn dither
+  "Random tie breaker"
+  []
+  (let [choice (rand 3.0)]
+    (cond
+     (< choice 1) '(0.05 0 0)
+     (< choice 2) '(0 0.05 0)
+     :else
+     '(0 0 0.05))))
+
+(defn pick_chord_w_more_rules [thenote]
+    "Returns pitch-classes of a chord."
+    (let [the_pc (rem thenote 12)
+          solSet [0 0 0]]
+      (reset! *as_root_set* (make_set (as_root the_pc)))
+      (reset! *as_third_set* (make_set (as_third the_pc)))
+      (reset! *as_fifth_set* (make_set (as_fifth the_pc)))
+      (reset! solSet (add_lists solSet (common_tones_test)))
+      (reset! solSet (add_lists solSet (last_sol_test)))
+      (reset! solSet (add_lists solSet (favor_root_for_tonic the_pc)))
+      (reset! solSet (add_lists solSet (dither)))
+      (case (ltop (reset! *last_sol* solSet))
+        (2 (as_fifth the_pc))
+        (1 (as_third the_pc))
+        :else
+        (as_root the_pc))))
+
+(defn add_oct
+  "Adds octave to the list."
+  ([theList] (add_oct theList 4))
+  ([theList the-octave]
+     (map (fn [x] (+ x (* the-octave 12)))
+             (ascend_list theList))))
+
+(defn make_chord_event
+  "Simple notelist to chord."
+  ([pitch_list] (make_chord_event pitch_list 0))
+  ([pitch_list time]
+      (if (empty? pitch_list) []
+          (cons (list time (first pitch_list) *default_dur* *default_ch* *default_vel*)
+                (make_chord_event (rest pitch_list) time)))))
+
+(defn pick_and_play_more_rules_chord [thenote time]
+    "Returns a chord in event notation."
+    (make_chord_event
+     (add_oct
+      (pick_chord_w_more_rules (rem thenote 12)) 4) time))
+
+(defn fuzzy
+  ([pcs] (fuzzy pcs 0))
+  ([pcs time]
+     (if (empty? pcs) []
+         (concat (pick_and_play_more_rules_chord (first pcs) time)
+                 (fuzzy (rest pcs) (+ time 1000))))))
