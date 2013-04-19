28a8e5f78a9d718c9755a70ffd12d8210550b950
josephwilk <joe@josephwilk.net>
2013-04-19 10:56:21 +0200

    new name

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject computer-models-of-musical-creativity "0.1.0-SNAPSHOT"
+(defproject musical-creativity "0.1.0-SNAPSHOT"
   :description "Examples from David Copes Computer Models of Musical Creativity book"
-  :url "http://github.com/josephwilk/computer-models-of-musical-creativity"
+  :url "http://github.com/josephwilk/musical-creativity"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone "0.8.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]]
