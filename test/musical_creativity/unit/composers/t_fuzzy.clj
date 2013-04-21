a976c9f387937a2a82a35bb10286b3f93ecf30ae
josephwilk <joe@josephwilk.net>
2013-04-21 19:01:31 +0200

    checking n-top behaviour

--- test/musical_creativity/unit/composers/t_fuzzy.clj
+++ test/musical_creativity/unit/composers/t_fuzzy.clj
@@ -11,3 +11,6 @@
 
 (fact "fz-union"
   (fuzzy/fz-union '(1 0 0 0 0 0 0 0 0 0 0 0) '(0 0 0 0 0 0 0 0 0 0 0 0)) => [1 0 0 0 0 0 0 0 0 0 0 0])
+
+(fact "top-n-positions"
+  (fuzzy/top-n-positions [1 2 3 4 5 6] 3) => [5 4 3])
