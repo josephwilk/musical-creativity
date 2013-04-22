6e004731a890c6892f53b431727725e0d5ec6e24
josephwilk <joe@josephwilk.net>
2013-04-22 12:05:42 +0200

    New instruments

--- src/musical_creativity/instruments.clj
+++ src/musical_creativity/instruments.clj
@@ -13,6 +13,21 @@
      (saw freq)
      vol))
 
+(definst kick [freq 120 dur 0.3 width 0.5]
+  (let [freq-env (* freq (env-gen (perc 0 (* 0.99 dur))))
+        env (env-gen (perc 0.01 dur) 1 1 0 1 FREE)
+        sqr (* (env-gen (perc 0 0.01)) (pulse (* 2 freq) width))
+        src (sin-osc freq-env)
+        drum (+ sqr (* env src))]
+    (compander drum drum 0.2 1 0.1 0.01 0.01)))
+
+(definst c-hat [amp 0.8 t 0.04]
+  (let [env (env-gen (perc 0.001 t) 1 1 0 1 FREE)
+        noise (white-noise)
+        sqr (* (env-gen (perc 0.01 0.04)) (pulse 880 0.2))
+        filt (bpf (+ sqr noise) 9000 0.5)]
+    (* amp env filt)))
+
 (def g (stringed/guitar))
 
 (defn- saw2 [music-note]
