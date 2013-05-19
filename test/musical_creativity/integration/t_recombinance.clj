a5493a3ff8564cc912760af646fc36b1ee420c21
Joseph Wilk <joe@josephwilk.net>
2013-05-19 13:05:21 +0200

    db is loaded explicitly now.

--- test/musical_creativity/integration/t_recombinance.clj
+++ test/musical_creativity/integration/t_recombinance.clj
@@ -1,8 +1,9 @@
 (ns musical-creativity.integration.t-recombinance
   (:require
    [midje.sweet                               :refer :all]
+   [data.bach                                 :as bach]
    [musical-creativity.composers.recombinance :refer :all]))
 
 (fact "it composes"
-  (load-bach-chorales)
+  (create-database-from bach/chorale-140-data)
   (count (compose)) => 197)
