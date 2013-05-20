f71af12f90fd57da609c88965eb24d808038c3dd
Joseph Wilk <joe@josephwilk.net>
2013-05-20 11:48:37 +0200

    adding kibit

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone "0.8.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]]
-                 :profiles {:dev {:dependencies [[midje "1.5.1"]
-                                                 [jonase/kibit "0.0.3"]
-                                                 [jonase/eastwood "0.0.2"]]
-                                  :plugins [[lein-midje "3.0.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-kibit "0.0.8"]
+                             [lein-midje "3.0.0"]]}})
