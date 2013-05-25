e8623ecd6e7cfff2fc1cd9bc916247accd9b8b39
Joseph Wilk <joe@josephwilk.net>
2013-05-25 16:52:26 +0200

    Exploring name generation

--- test/musical_creativity/unit/composers/t_improvise.clj
+++ test/musical_creativity/unit/composers/t_improvise.clj
@@ -7,7 +7,7 @@
 (fact "creating a database"
   (create-a-complete-database '(forgray))
 
-  (first (:grouping-names  (@*lexicon-store* "lexicon-87-41-41"))) => #"forgray\[\d+\]"
+  (first (:grouping-names (@*lexicon-store* "lexicon-87-41-41"))) => #"forgray\[\d+\]"
   (@*groupings-store* "forgray[2]-97-99-41") => {:destination "forgray[2]-41-101-103",
                                                  :events '((59000 97 99 1 71) (59000 99 99 1 109) (59000 41 99 1 93 tie)),
                                                  :lexicon "lexicon-97-99-41",
@@ -16,3 +16,14 @@
 
 (fact "making lexicon name"
   (make-name-of-lexicon '(63 59 37)) => "lexicon-63-59-37")
+
+(fact "removing ends"
+  (create-a-complete-database '(forgray))
+
+  ;(reset! *lexicon-store* {})
+
+  (remove-ends '(lexicon-77-36 lexicon-77-36-74 lexicon-74-70-34)) =>
+  '(lexicon-77-36 lexicon-77-36-74 lexicon-74-70-34))
+
+(fact "choose begining grouping"
+  (choose-beginning-grouping '("fourbros [110]-62-65-68-44-41-51")) => "fourbros [110]-62-65-68-44-41-51")
