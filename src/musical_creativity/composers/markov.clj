16ff6f4d8e9fc80333f8bac9c1bd6bbaed4a755b
Maximilian Karasz <maximilian.karasz@googlemail.com>
2013-10-18 17:11:30 +0200

    add phrase length parameter.
    now the model can transition to phrases of notes.

--- src/musical_creativity/composers/markov.clj
+++ src/musical_creativity/composers/markov.clj
@@ -20,6 +20,7 @@
   {:start [60]
    :events default-events
    :length 50
+   :phrase-length 1
    :depth 1})
 
 (defn pick-pitches-fn [depth stm]
@@ -28,31 +29,33 @@
           candidates (stm last-chunk)
           picked (rand-nth candidates)]
       (if picked
-        (conj pitches picked)
+        (concat pitches picked)
         pitches))))
 
 (defn- compose-pitches [start length depth stm]
   (nth (iterate (pick-pitches-fn depth stm) start) length))
 
-(defn probabilities-for [stm chunk]
-  (let [prefix (drop-last chunk)
-        suffix (last chunk)
+(defn probabilities-for [depth stm chunk]
+  (let [prefix  (take depth chunk)
+        suffix  (drop depth chunk)
         stm-row (or (stm prefix) [])]
     (assoc stm prefix (conj stm-row suffix))))
 
-(defn- state-transition-matrix-probabilities [pitches depth]
-  (let [pitch-chunks (partition (inc depth) 1 pitches)]
-    (reduce probabilities-for {} pitch-chunks)))
+(defn- state-transition-matrix-probabilities [pitches depth phrase-length]
+  (let [pitch-chunks (partition (+ depth phrase-length) 1 pitches)]
+    (reduce (partial probabilities-for depth) {} pitch-chunks)))
 
 (defn- compose-markov [start length depth stm]
   (events/make (flatten (compose-pitches start length depth stm)) 0 350))
 
 (defn compose
-  ([] (compose (:events defaults) (:start defaults) (:length defaults)))
-  ([events start length]
+  ([] (compose (:events defaults) (:start defaults) (:phrase-length defaults) (:length defaults)))
+  ([events start phrase-length length]
+     (assert (zero? (mod length phrase-length))
+             (format "length %s is no multiple of phrase-length %s" length phrase-length))
      (let [depth (count start)]
        (if (= (count start) depth)
          (let [pitches (map :pitch events)
-               stm (state-transition-matrix-probabilities pitches depth)]
-           (compose-markov start length depth stm))
+               stm (state-transition-matrix-probabilities pitches depth phrase-length)]
+           (compose-markov start (/ length phrase-length) depth stm))
          (println (str "Error: Start sequence must be same as depth: start:" (count start) " depth:" depth))))))
