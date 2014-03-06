6d2b676dd144c35505571956544e7ca93acd3f3e
Nada Amin <nada.amin@epfl.ch>
2014-03-06 11:18:10 +0100

    Update to latest deps

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject musical-creativity "0.1.0-SNAPSHOT"
   :description "Examples from David Copes Computer Models of Musical Creativity book"
   :url "http://github.com/josephwilk/musical-creativity"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [overtone "0.8.1"]
-                 [org.clojure/math.numeric-tower "0.0.2"]
-                 [cheshire "5.2.0"]
-                 [clj-http "0.7.6"]
-                 [quil "1.6.0"]
-                 [aysylu/loom "0.3.1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone "0.9.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [cheshire "5.3.1"]
+                 [clj-http "0.9.0"]
+                 [quil "1.7.0"]
+                 [aysylu/loom "0.4.2"]
                  [midi-mash "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+  :profiles {:dev {:dependencies [[midje "1.6.2"]]
                    :plugins [[lein-kibit "0.0.8"]
-                             [lein-midje "3.0.0"]]}})
+                             [lein-midje "3.1.3"]]}})
